0cc209013535fd894e07b1976545a5a9bdd9e809
pvsbreddy06 <pvsbreddy06@gmail.com>
2026-02-06 19:25:04 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,19 @@
+function loadPath() {
+  const dummyData = {
+    skills: { python: 2 },
+    target_role: "data_scientist",
+    time_per_week: 5
+  };
+
+  fetch("http://127.0.0.1:5000/assess", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dummyData)
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("output").innerText =
+      JSON.stringify(data, null, 2);
+  });
+}
 
